db8f136995f5d63f611f6f70f8edfa0830d830f3
axhmed <ahmed_abdalla405@yahoo.com>
2022-05-23 13:52:29 +0200

    removed debug.h from gitignore

---
+++ debug.h
@@ -0,0 +1,10 @@
+#ifndef _DEBUG_
+#define _DEBUG_
+
+static bool legInverseKinematicsDebugFlag = false;
+static bool linearTrajectoryFlag = false;
+static bool arcTrajectoryFlag = false;
+static bool elipticalTrajectoryFlag = false;
+static bool motionProcessFlag = false;
+
+#endif
